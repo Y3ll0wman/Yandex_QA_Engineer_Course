9233b50e636f51ecaf2918ef37ad8c1082a6e8fe
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-03-31 21:35:29 +0300

    Part one - сompleted

---
+++ Part-1-Introduction-to-JavaScript/Lesson-3-Variables-assignment-operation.js
@@ -0,0 +1,17 @@
+/*
+Объяви константу car, переменные bus и carSharing так, чтобы:
+в car содержалось значение 'Mercedes',
+в bus — 'Volvo',
+а переменная carSharing содержала значение из car.
+*/
+
+const car = 'Mercedes',
+let bus = 'Volvo';
+let carSharing = car;
+
+// Выведи в консоль значения переменных car, bus и carSharing.
+
+const car = 'Mercedes';
+let bus = 'Volvo';
+let carSharing = car;
+console.log(car,bus,carSharing);
---
+++ Part-1-Introduction-to-JavaScript/Lesson-4-Elementary-Data-Types.js
@@ -0,0 +1,12 @@
+/*
+У машины турбированный движок мощностью 249 лошадиных сил. Максимальная скорость — 260 км/ч. Объяви переменные engine, turbo, maxSpeed. Присвой им значения '249 hp', true , 260 соответственно.
+Выведи значения переменных и их типы.
+*/
+
+let engine = '249 hp';
+let turbo = true;
+let maxSpeed = 260;
+// выведи переменные engine, turbo и maxSpeed
+console.log(engine, turbo, maxSpeed);
+// выведи типы переменных engine, turbo и maxSpeed
+console.log(typeof engine,typeof turbo,typeof maxSpeed);
